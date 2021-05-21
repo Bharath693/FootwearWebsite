a652436698ae52df690b04f8e804f9e2a772c8be
bharath <bharathraj693@gmail.com>
2021-05-21 12:01:01 +0530

    Fetching data from redux has some bug

--- src/ReduxStore/ReduxStore.js
+++ src/ReduxStore/ReduxStore.js
@@ -2,10 +2,10 @@ import {   combineReducers , applyMiddleware, createStore} from 'redux'
 import thunk from 'redux-thunk'
 import { persistReducer , persistStore} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
-import  ProductsReducer  from '../Global/ProductsReducer'
+import   {ProductsReducer}   from '../Global/ProductsReducer'
 
 
- const rootReducer = combineReducers({ ProductsReducer })
+ const rootReducer = combineReducers( { ProductsReducer } )
 
 const persistConfig = {
     key:"root",
